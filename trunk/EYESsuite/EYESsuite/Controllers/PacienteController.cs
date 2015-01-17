1e953be731564ade6904e12981be9cc5bb65636e
eberttc@gmail.com <eberttc@gmail.com@af6b478f-af37-deb1-d7bd-5b05695e7097>
2015-01-17 06:01:08 +0000

    creacion de proyecto

---
+++ trunk/EYESsuite/EYESsuite/Controllers/PacienteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EYESsuite.Controllers
+{
+    public class PacienteController : Controller
+    {
+        //
+        // GET: /Paciente/
+
+
+
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            ViewBag.flagExito = false;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(String viewmodel="asd")
+        {
+            ViewBag.flagExito = true;
+            return View();
+        }
+        
+        public ActionResult Index()
+        {
+
+            
+            return View();
+        }
+    }
+}

6d2c66034fc7311db3c95ed28884d32c286ac661
eberttc <eberttc@gmail.com>
2015-01-17 06:01:08 +0000

    creacion de proyecto

---
+++ EYESsuite/EYESsuite/Content/js/custom.js
@@ -0,0 +1,32 @@
+function TestTable1(){
+	$('#datatable-1').dataTable( {
+		"aaSorting": [[ 0, "asc" ]],
+		"sDom": "<'box-content'<'col-sm-6'f><'col-sm-6 text-right'l><'clearfix'>>rt<'box-content'<'col-sm-6'i><'col-sm-6 text-right'p><'clearfix'>>",
+		"sPaginationType": "bootstrap",
+		"oLanguage": {
+			"sSearch": "",
+			"sLengthMenu": '_MENU_'
+		}
+	});
+}
+
+
+function LoadDataTablesScripts(callback){
+	function LoadDatatables(){
+		$.getScript('plugins/datatables/jquery.dataTables.js', function(){
+			$.getScript('plugins/datatables/ZeroClipboard.js', function(){
+				$.getScript('plugins/datatables/TableTools.js', function(){
+					$.getScript('plugins/datatables/dataTables.bootstrap.js', callback);
+				});
+			});
+		});
+	}
+	if (!$.fn.dataTables){
+		LoadDatatables();
+	}
+	else {
+		if (callback && typeof(callback) === "function") {
+			callback();
+		}
+	}
+}
---
+++ EYESsuite/EYESsuite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EYESsuite.MvcApplication" Language="C#" %>

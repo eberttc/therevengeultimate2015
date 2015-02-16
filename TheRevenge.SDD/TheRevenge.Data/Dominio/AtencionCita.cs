71f4beedbc22eb44833ae28bed23e1f4a9a78705
miguel.merinob <miguel.merinob@gmail.com>
2015-02-16 22:34:01 +0000

    Clases AtencionCita.Cs y AtencionCita.hbm.xml

---
+++ TheRevenge.SDD/TheRevenge.Data/Dominio/AtencionCita.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRevenge.Data.Dominio
+{
+    public class AtencionCita
+    {
+        public int IdCita { get; set; }
+        public DateTime FechaAtencion { get; set; }
+        public string Diagnostico { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
